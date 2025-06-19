977d353ca2c7af27d1f425b146c83f78ff90182e
Bhushan Bhale <bhushan27official@gmail.com>
2025-06-19 19:57:01 +0530

    EP2

--- EP2_Ignition_our_app/index.js
+++ EP2_Ignition_our_app/index.js
@@ -1,5 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1", {}, "hello from h3");
+
+// const heading = React.createElement("h1", {}, "hello react here...");
+// console.log(heading);
+
+// const jsxHeading = (
+//   <h1 tabIndex="5" className="heading">
+//     Hello JSX
+//   </h1>
+// );
+
+// use below this is better
+const HeadingComponent = <h1>This is from react element</h1>;
+console.log(HeadingComponent);
+
+const Title = () => (
+  <div>
+    <h4>This is title</h4>
+  </div>
+);
+
+const HeadingComponent2 = () => (
+  <div>
+    {console.log("Bhushan js inside JSX")}
+    <h1>Bhushan from h1 in div ddfdfdf dfgdgdfg</h1>
+  </div>
+);
+
+const App = () => (
+  <>
+    <Title />
+    <HeadingComponent2 />
+  </>
+);
+
+console.log(HeadingComponent2());
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<App />);
